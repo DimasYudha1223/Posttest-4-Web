ce5516d271f7aeb52522057c5e828aaeb88e5114
STressNGoding <93185675+DimasYudha1223@users.noreply.github.com>
2022-10-21 05:42:29 +0800

    Delete Posttest 5/crud directory

--- Posttest 5/crud/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname="localhost";
-$username="root";
-$password=" ";
-$database="db_kampus";
-$koneksi= mysql_connect($hostname,$username,$password) or die ("koneksi ke MYSQL gagal");
-mysql_select_db($database,$koneksi) or die ("koneksi ke database gagal");
-?>
